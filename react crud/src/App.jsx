3c5df1b52487491e2628709458287370951b5266
ridho <totti.prtama@gmail.com>
2024-12-06 14:16:06 +0700

    jul

--- react crud/src/App.jsx
+++ react crud/src/App.jsx
@@ -9,24 +9,24 @@ function App() {
 
   return (
     <Router>
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
                 <NavLink to="/" className="nav-link">Home</NavLink>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <NavLink to="/fakultas" className="nav-link">Fakultas</NavLink>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <NavLink to="/prodi" className="nav-link">Program Studi</NavLink>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <NavLink to="/mhs" className="nav-link">Mahasiswa</NavLink>
               </li>
             </ul>
--- react crud/src/components/Fakultas/List.jsx
+++ react crud/src/components/Fakultas/List.jsx
@@ -1,5 +1,19 @@
-export default function List(){
-    return(
+import React, { useEffect, useState } from "react"
+import axios from 'axios'
+export default function List() {
+    //state fakultas
+    const [fakultas, setFakultas] = useState([]);
+
+    useEffect(()=>{
+        axios
+        .get("https://laravel-mi-5-a-iota.vercel.app/api/api/fakultas")
+        .then((response) =>{
+            console.log(response)
+            setFakultas(response.data.result)  
+        })
+    },[]) 
+
+    return (
         <>
             <h2>List Fakultas</h2>
         </>
